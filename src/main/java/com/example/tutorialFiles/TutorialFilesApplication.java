7138e4b09a6ba2ad80a9e808e2eafd277c843d43
Alexander Luque <114216624+alexluque02@users.noreply.github.com>
2024-02-12 12:00:08 +0100

    Ejemplo hecho

---
+++ src/main/java/com/example/tutorialFiles/TutorialFilesApplication.java
@@ -0,0 +1,26 @@
+package com.example.tutorialFiles;
+
+import com.example.tutorialFiles.service.StorageProperties;
+import com.example.tutorialFiles.service.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class TutorialFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialFilesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
---
+++ src/test/java/com/example/tutorialFiles/TutorialFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorialFiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
